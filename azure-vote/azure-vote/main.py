6597bd864bc7e37391309aef4cb8c797b0323139
Winterfellwen <38210291+Winterfellwen@users.noreply.github.com>
2023-06-28 15:32:33 +0800

    Update main.py

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess
+
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def index():
-    return "<h1 style='color: blue'>Hello World!!!</h1>"
- 
-if __name__ == "__main__":
+    return render_template('index.html')
+
+@app.route('/get_vm')
+def get_vm():
+    result = subprocess.run(['pwsh', '-c', 'Connect-AzAccount; Get-AzVM'], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return f"Error: {result.stderr}"
+
+if __name__ == '__main__':
     app.run()
